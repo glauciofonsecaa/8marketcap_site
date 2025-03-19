993ef8f36cb08131c5c53fd4dd90a874b56bd85e
Glaucio Fonseca <163554650+glauciofonsecaa@users.noreply.github.com>
2025-03-19 12:23:18 -0300

    Add files via upload

--- 8marketcap_site.py
+++ 8marketcap_site.py
@@ -1,32 +1,38 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup as bs
 
+# URL da página
 url = 'https://8marketcap.com/'
+
+# Requisição HTTP para obter o conteúdo da página
 response = requests.get(url)
 
-def gold():
-    symbol = soup.select('#default-table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(3) > div:nth-child(2) > div:nth-child(1)')[0].text
-    market_cap = soup.select('#default-table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(5)')[0].text.replace('T', '')
-    price = soup.select('#default-table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(6)')[0].text
-    var_24h = soup.select('#default-table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(7) > span:nth-child(1)')[0].text.strip()
-    var_7d = soup.select('#default-table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(8) > span:nth-child(1)')[0].text.strip()
-    
-    return symbol, market_cap, price, var_24h, var_7d
-
-def apple():
-    symbol = soup.select('#default-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(3) > div:nth-child(2) > div:nth-child(1)')[0].text
-    market_cap = soup.select('#default-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(5)')[0].text.replace('T', '')
-    price = soup.select('#default-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(6)')[0].text
-    var_24h = soup.select('#default-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(7) > span:nth-child(1)')[0].text.strip()
-    var_7d = soup.select('#default-table > tbody:nth-child(2) > tr:nth-child(2) > td:nth-child(8) > span:nth-child(1)')[0].text.strip()
-    
-    return symbol, market_cap, price, var_24h, var_7d
-
+# Verifica se a resposta foi bem-sucedida
 if response.status_code == 200:
     soup = bs(response.content, 'html.parser')
-    print(gold()[1])
-    print(apple()[0])
-    print(apple()[1])
+    
+    # Função para obter dados
+    def get_data(row):
+        symbol = soup.select(f'#default-table > tbody:nth-child(2) tr:nth-child({row}) > td:nth-child(4) > span:nth-child(1)')[0]
+        market_cap = soup.select(f'#default-table > tbody:nth-child(2) > tr:nth-child({row}) > td:nth-child(5)')[0]
+        price = soup.select(f'#default-table > tbody:nth-child(2) > tr:nth-child({row}) > td:nth-child(6)')[0]
+        var_24h = soup.select(f'#default-table > tbody:nth-child(2) > tr:nth-child({row}) > td:nth-child(7) > span:nth-child(1)')[0]
+        var_7d = soup.select(f'#default-table > tbody:nth-child(2) > tr:nth-child({row}) > td:nth-child(8) > span:nth-child(1)')[0]
+        
+        return symbol, market_cap, price, var_24h, var_7d
+
+    # Coletar dados
+    gold_data = get_data(1)
+    apple_data = get_data(2)
+    microsoft = get_data(3)
+    nvidia = get_data(4)
+
+    # Criar DataFrame
+    df = pd.DataFrame([gold_data, apple_data, microsoft, nvidia], columns=['Symbol', 'Market Cap (T)', 'Price', '24h Change', '7d Change'])
+
+    # Exibir DataFrame
+    print(df)
 
 else:
-    print(f'Failure: {response.status_code}')
+    print(f'Falha ao acessar o site. Código de status: {response.status_code}')
